b6a1a903f29ee76e67dc1315a09148d47a7b5a36
root <root@insa-21131.insa-toulouse.fr>
2024-10-23 07:56:35 +0000

    tp2

--- tp2/tp.c
+++ tp2/tp.c
@@ -1,17 +1,34 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
-
+#include <intr.h>
 void bp_handler() {
    // TODO
+ 	  uint32_t val;
+   asm volatile ("mov 4(%%ebp), %0":"=r"(val));
+	debug("Val = %x\n",val);
+  	debug("bp_handler activated !!!\n");
+//	asm volatile ("popa"); //Sauvegarde eax,ebx... dans la pile
+//	asm volatile ("leave; iret");
 }
 
 void bp_trigger() {
-	// TODO
+	asm volatile ("int3");
+	debug("End bp_trigger\n");
 }
 
 void tp() {
 	// TODO print idtr
+	idt_reg_t idt;
+	get_idtr(idt);
+	debug("IDTR addr : 0x%x ",(unsigned int) idt.addr);
+	debug("limit : %d\n", idt.limit);
 
-	// TODO call bp_trigger
-   //bp_trigger();
+
+	int_desc_t *my_idt = &idt.desc[3];
+
+
+	my_idt->offset_1 = (uint16_t)((uint32_t)bp_handler);
+	my_idt->offset_2 = (uint16_t)(((uint32_t)bp_handler)>>16);
+
+   bp_trigger();
 }
